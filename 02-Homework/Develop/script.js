81e2806770641fb0e23f40af0ec8659adee1eab2
Jason Day <jason.day.blue@gmail.com>
2021-11-11 23:56:30 -0600

    in progress

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,13 +1,32 @@
+//Prompt
+promptWindow = window.alert(
+	"click the red button to generate a password. Password can be between 8-128 characters in length. A-Z,0-9, and including special characters"
+);
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
+	let password = generatePassword();
+	let passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+let generatePassword = function () {
+	let array =
+		"1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM !#$%&'()*+,-./:;<=>?@[^_`{|}~";
+	array.split("");
+	console.log(array.split(""));
+
+	for (let i = 0; i < 8; i++) {
+		let pw = [];
+		pw += array[i];
+		return pw.toString();
+	}
+};
